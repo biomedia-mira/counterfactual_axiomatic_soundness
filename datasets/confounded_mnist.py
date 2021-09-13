f2019ca05a754b657004e8b50445abd981ecebb1
MiguelMonteiro <mab.mtr@gmail.com>
2021-09-13 13:17:54 +0100

    bug fixes

--- datasets/confounded_mnist.py
+++ datasets/confounded_mnist.py
@@ -1,5 +1,5 @@
-from functools import partial
-from typing import Callable, Dict, List, Tuple, Any
+import os
+from typing import Callable, Dict, List, Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -10,11 +10,36 @@ from scipy.ndimage import gaussian_filter
 from datasets.jpeg import get_jpeg_encode_decode_fns
 from datasets.morphomnist.perturb import ImageMorphology, Swelling
 from datasets.utils import get_unconfounded_datasets, image_gallery
+from pathlib import Path
 
 tf.config.experimental.set_visible_devices([], 'GPU')
 
 MechanismFn = Callable[[tf.Tensor, tf.Tensor, Dict[str, tf.Tensor]], Tuple[tf.Tensor, tf.Tensor, Dict[str, tf.Tensor]]]
+##
+from skimage import transform
 
+from datasets.morphomnist import skeleton
+from datasets.morphomnist.morpho import ImageMorphology
+
+
+def warp(xy: np.ndarray, morph: ImageMorphology, strength, radius) -> np.ndarray:
+    loc_sampler = skeleton.LocationSampler()
+    centre = loc_sampler.sample(morph)[::-1]
+    radius = (radius * np.sqrt(morph.mean_thickness) / 2.) * morph.scale
+    offset_xy = xy - centre
+    distance = np.hypot(*offset_xy.T)
+    weight = (distance / radius) ** (strength - 1)
+    weight[distance > radius] = 1.
+    return centre + weight[:, None] * offset_xy
+
+
+def ___swell(image: np.ndarray, strength: float = 3, radius: float = 7):
+    assert image.ndim == 3 and image.shape[-1] == 1
+    morph = ImageMorphology(image[..., 0])
+    return np.expand_dims(transform.warp(image[..., 0], lambda xy: warp(xy, morph, strength, radius)), axis=-1)
+
+
+##
 
 def get_jpeg_encoding_decoding_fns(max_seq_len: int, image_shape: Tuple[int, int, int]):
     encode_fn, decode_fn = get_jpeg_encode_decode_fns(max_seq_len=max_seq_len, block_size=(8, 8), quality=50,
@@ -60,48 +85,61 @@ def get_diagonal_confusion_matrix(num_rows: int, num_columns: int, noise: float 
             num_rows - 1)
 
 
-def get_mechanism_fn(parent_name: str, digit: np.ndarray, cm: np.ndarray,
-                     functions: List[Callable[[tf.Tensor], tf.Tensor]]):
-    assert cm.shape[1] == len(functions)
+def get_colorize_fn(digit: np.ndarray, cm: np.ndarray):
     indices = tf.constant(list(map(lambda label: np.random.choice(cm.shape[1], p=cm[label]), digit)), dtype=tf.int64)
+    colors = tf.constant(((1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 0), (1, 0, 1),
+                          (0, 1, 1), (1, 1, 1), (.5, 0, 0), (0, .5, 0), (0, 0, .5)))
 
     def apply_fn(index: tf.Tensor, image: tf.Tensor, parents: Dict[str, tf.Tensor]) \
             -> Tuple[tf.Tensor, tf.Tensor, Dict[str, tf.Tensor]]:
         idx = indices[index]
-        image = tf.switch_case(tf.cast(idx, tf.int32), {i: partial(fn, image=image) for i, fn in enumerate(functions)})
-        return index, image, {**parents, parent_name: idx}
+        color = colors[idx]
+        image = tf.concat([image] * 3, axis=-1) * tf.reshape(color, (1, 1, 3))
+        return index, image, {**parents, 'color': idx}
 
-    return apply_fn
-
-
-def colorize(image: tf.Tensor, color: tf.Tensor) -> tf.Tensor:
-    return tf.concat([image] * 3, axis=-1) * tf.reshape(color, (1, 1, 3))
+    return apply_fn, {'color': indices.numpy()}
 
 
-@tf.function(input_signature=[tf.TensorSpec(None, tf.float32)])
-def swell(image: tf.Tensor, sigma: float = .5) -> tf.Tensor:
-    def _swell(_image: np.ndarray) -> Any:
-        return np.expand_dims(
-            gaussian_filter(Swelling()(ImageMorphology(_image[..., 0])).astype(np.float32), sigma=sigma), -1)
+def get_swelling_fn(digit: np.ndarray, cm: np.ndarray):
+    indices = tf.constant(list(map(lambda label: np.random.choice(cm.shape[1], p=cm[label]), digit)), dtype=tf.int64)
 
-    return tf.numpy_function(_swell, [image], tf.float32)
+    def warp(xy: np.ndarray, morph: ImageMorphology, strength, radius) -> np.ndarray:
+        loc_sampler = skeleton.LocationSampler()
+        centre = loc_sampler.sample(morph)[::-1]
+        radius = (radius * np.sqrt(morph.mean_thickness) / 2.) * morph.scale
+        offset_xy = xy - centre
+        distance = np.hypot(*offset_xy.T)
+        weight = (distance / radius) ** (strength - 1)
+        weight[distance > radius] = 1.
+        return centre + weight[:, None] * offset_xy
+
+    def swell(image: np.ndarray, strength: float = 3, radius: float = 7):
+        assert image.ndim == 3 and image.shape[-1] == 1
+        morph = ImageMorphology(image[..., 0])
+        return np.expand_dims(transform.warp(image[..., 0], lambda xy: warp(xy, morph, strength, radius)), axis=-1)
+
+    def apply_fn(index: tf.Tensor, image: tf.Tensor, parents: Dict[str, tf.Tensor]) \
+            -> Tuple[tf.Tensor, tf.Tensor, Dict[str, tf.Tensor]]:
+        idx = indices[index]
+        image = tf.cond(idx == 0, lambda: image, lambda: tf.numpy_function(swell, [image], tf.float32))
+        return index, image, {**parents, 'swell': idx}
+
+    return apply_fn, {'swell': indices.numpy()}
 
 
-def get_mechanisms(digit: np.ndarray) -> List[MechanismFn]:
-    colors = tf.constant(((1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 0), (1, 0, 1),
-                          (0, 1, 1), (1, 1, 1), (.5, 0, 0), (0, .5, 0), (0, 0, .5)))
-    train_colorize_cm = get_diagonal_confusion_matrix(10, 10, noise=.1)
-    test_colorize_cm = get_uniform_confusion_matrix(10, 10)
-    colorize_fns = [partial(colorize, color=color) for color in colors]
-    colorize_fn = get_mechanism_fn('color', digit, train_colorize_cm, colorize_fns)
-
+def get_mechanisms(digit: np.ndarray) -> Tuple[List[MechanismFn], Dict[str, np.ndarray]]:
     # even digits have much higher chance of swelling
     train_swell_cm = np.zeros(shape=(10, 2))
     train_swell_cm[0:-1:2] = (.1, .9)
     train_swell_cm[1::2] = (.9, .1)
-    swelling_fn = get_mechanism_fn('swell', digit, train_swell_cm, [lambda x: x, swell])
+    swelling_fn, swell = get_swelling_fn(digit, train_swell_cm)
 
-    return [colorize_fn]
+    train_colorize_cm = get_diagonal_confusion_matrix(10, 10, noise=.1)
+    test_colorize_cm = get_uniform_confusion_matrix(10, 10)
+    colorize_fn, color = get_colorize_fn(digit, train_colorize_cm)
+
+    parents_np = {'digit': digit, **swell, **color}
+    return [swelling_fn, colorize_fn], parents_np
 
 
 def apply_mechanisms_to_dataset(dataset: tf.data.Dataset, mechanisms: List[MechanismFn]) -> tf.data.Dataset:
@@ -113,32 +151,43 @@ def apply_mechanisms_to_dataset(dataset: tf.data.Dataset, mechanisms: List[Mecha
 
 
 def create_confounded_mnist_dataset(batch_size: int, debug: bool = True, jpeg_encode: bool = True):
-    ds_train, ds_test = tfds.load('mnist', split=['train', 'test'], shuffle_files=True, as_supervised=True)
-    parent_dims = {'digit': 10, 'color': 10}
+    cache_dir = Path('./data/confounded_mnist')
+    parent_dims = {'digit': 10, 'swell': 2, 'color': 10}
     image_shape = (28, 28, 3)
     seq_len = 350
     seq_shape = (seq_len, 4)
-    train_targets = np.array([y for x, y in ds_train.as_numpy_iterator()])
-    test_targets = np.array([y for x, y in ds_train.as_numpy_iterator()])
+
+    if not os.path.exists(cache_dir):
+        ds_train, ds_test = tfds.load('mnist', split=['train', 'test'], shuffle_files=False, as_supervised=True)
+        train_targets = np.array([y for x, y in ds_train.as_numpy_iterator()])
+        test_targets = np.array([y for x, y in ds_train.as_numpy_iterator()])
+        # Confound the dataset artificially
+        mechanisms, parents_np = get_mechanisms(train_targets)
+        dataset = apply_mechanisms_to_dataset(ds_train, mechanisms)
+        tf.data.experimental.save(dataset, str(cache_dir), compression='GZIP')
+        np.save(str(cache_dir / 'parents.npy'), parents_np)
+    else:
+        dataset = tf.data.experimental.load(str(cache_dir), compression='GZIP')
+        parents_np = np.load(str(cache_dir / 'parents.npy'), allow_pickle=True).item()
 
     if jpeg_encode:
-        img_encode_fn, img_decode_fn = get_jpeg_encoding_decoding_fns(seq_len, image_shape)
-        input_shape = (-1, *seq_shape)
+        raise NotImplementedError
+        # img_encode_fn, img_decode_fn = get_jpeg_encoding_decoding_fns(seq_len, image_shape)
+        # input_shape = (-1, *seq_shape)
     else:
-        img_encode_fn, img_decode_fn = rgb_encode_fn, rgb_decode_fn
-        input_shape = (-1, *image_shape)
-
-    # Confound the dataset artificially
-    mechanisms = get_mechanisms(train_targets)
-    dataset = apply_mechanisms_to_dataset(ds_train, mechanisms)
+        img_encode_fn, img_decode_fn, input_shape = rgb_encode_fn, rgb_decode_fn, (-1, *image_shape)
 
     # Get unconfounded datasets by looking at the parents
-    cache_filename = '/tmp/cached_confounded_mnist'
-    dataset, marginals = get_unconfounded_datasets(dataset, parent_dims, batch_size, img_encode_fn, cache_filename)
+    dataset, marginals = get_unconfounded_datasets(dataset, parents_np, parent_dims, batch_size, img_encode_fn)
 
     if debug:
-        for key, data in iter(dataset).__next__().items():
-            order = np.argsort(np.argmax(data[1]['digit'], axis=1))
-            plt.imshow(image_gallery(img_decode_fn(data[0])[order]))
-            plt.show()
+        flag = False
+        for el in dataset:
+            if not flag:
+                for key, data in el.items():
+                    order = np.argsort(np.argmax(data[1]['digit'], axis=1))
+                    plt.imshow(image_gallery(img_decode_fn(data[0])[order], num_images_to_display=256))
+                    plt.title(str(key))
+                    plt.show()
+                flag = True
     return dataset, parent_dims, marginals, img_decode_fn, input_shape
--- datasets/utils.py
+++ datasets/utils.py
@@ -1,5 +1,5 @@
 import itertools
-from typing import Dict, Tuple, Callable, Optional
+from typing import Dict, Tuple, Callable
 
 import numpy as np
 import tensorflow as tf
@@ -7,9 +7,9 @@ import tensorflow_datasets as tfds
 from more_itertools import powerset
 
 
-def image_gallery(array: np.ndarray, ncols: int = 8, num_images_to_display: int = 128) -> np.ndarray:
+def image_gallery(array: np.ndarray, ncols: int = 16, num_images_to_display: int = 128) -> np.ndarray:
     array = np.clip(array, a_min=0, a_max=255) / 255.
-    array = array[::len(array)//num_images_to_display]
+    array = array[::len(array) // num_images_to_display]
     nindex, height, width, intensity = array.shape
     nrows = nindex // ncols + int(bool(nindex % ncols))
     pad = np.zeros(shape=(nrows * ncols - nindex, height, width, intensity))
@@ -20,45 +20,47 @@ def image_gallery(array: np.ndarray, ncols: int = 8, num_images_to_display: int 
     return result
 
 
-def get_resample_fn(num_repeats: tf.Tensor) -> Callable[[tf.Tensor, tf.Tensor, Dict[str, tf.Tensor]], tf.data.Dataset]:
+def get_resample_fn(num_repeats: tf.Tensor, parent_dims: Dict[str, int]) -> Callable[
+    [tf.Tensor, tf.Tensor, Dict[str, tf.Tensor]], tf.data.Dataset]:
     def resample_fn(index: tf.Tensor, image: tf.Tensor, parents: Dict[str, tf.Tensor]) -> tf.data.Dataset:
-        return tf.data.Dataset.from_tensors((index, image, parents)).repeat(num_repeats[list(parents.values())])
+        _num_repeats = num_repeats[[parents[key] for key in parent_dims.keys()]]
+        return tf.data.Dataset.from_tensors((index, image, parents)).repeat(_num_repeats)
 
     return resample_fn
 
 
-def get_marginal_datasets(dataset: tf.data.Dataset, parent_dims: Dict[str, int]) \
+def get_marginal_datasets(dataset: tf.data.Dataset, parents: Dict[str, np.ndarray], parent_dims: Dict[str, int]) \
         -> Tuple[tf.data.Dataset, Dict[str, np.ndarray]]:
-    tmp = [item[-1] for item in dataset.as_numpy_iterator()]
-    parents = {key: np.array([item[key] for item in tmp]) for key in tmp[0].keys()}
     indicator = {key: [parents[key] == i for i in range(dim)] for key, dim in parent_dims.items()}
     index_map = np.array([np.logical_and.reduce(a) for a in itertools.product(*indicator.values())])
     index_map = index_map.reshape((*parent_dims.values(), -1))
     counts = np.sum(index_map, axis=-1)
+    if np.any(counts == 0):
+        raise ValueError('Distribution does not have full support.')
     joint_dist = counts / np.sum(counts)
     datasets, marginals = {}, {}
 
     for parent_set in powerset(parents.keys()):
         axes = tuple(np.flatnonzero(np.array([parent in parent_set for parent in parents])))
-        marginal_dist = np.sum(joint_dist, axis=tuple(set(range(counts.ndim)) - set(axes)), keepdims=True)
+        marginal_dist = np.ones(shape=(1,) * len(parents))
+        for axis in axes:
+            marginal_dist = marginal_dist * \
+                            np.sum(joint_dist, axis=tuple(set(range(counts.ndim)) - {axis}), keepdims=True)
+
         dist = marginal_dist * np.sum(joint_dist, axis=axes, keepdims=True)
         weights = dist / counts
         num_repeats = tf.convert_to_tensor(np.round(weights / np.min(weights)).astype(int))
-        datasets[frozenset(parent_set)] = dataset.flat_map(get_resample_fn(num_repeats))
-        if len(parent_set) == 1:
-            marginals[parent_set[0]] = np.squeeze(marginal_dist)
+        unconfounded_dataset = dataset.flat_map(get_resample_fn(num_repeats, parent_dims))
+        unconfounded_dataset = unconfounded_dataset.shuffle(buffer_size=np.sum(counts * num_repeats),
+                                                            reshuffle_each_iteration=True)
+        datasets[frozenset(parent_set)] = unconfounded_dataset
 
     return tf.data.Dataset.zip(datasets), marginals
 
 
-def get_unconfounded_datasets(dataset: tf.data.Dataset, parent_dims: Dict[str, int], batch_size: int, img_encode_fn,
-                              cache_filename: Optional[str] = '', buffer_size: int = 60000) \
-        -> Tuple[tf.data.Dataset, Dict[str, np.ndarray]]:
-
-    dataset, marginals = get_marginal_datasets(dataset, parent_dims)
-    dataset = dataset.cache(filename=cache_filename)
-    dataset = dataset.shuffle(buffer_size=buffer_size, reshuffle_each_iteration=True)
-    dataset = dataset.batch(batch_size, drop_remainder=True)
+def get_unconfounded_datasets(dataset: tf.data.Dataset, parents: Dict[str, np.ndarray], parent_dims: Dict[str, int],
+                              batch_size: int, img_encode_fn) -> Tuple[tf.data.Dataset, Dict[str, np.ndarray]]:
+    dataset, marginals = get_marginal_datasets(dataset, parents, parent_dims)
 
     def encode(*args):
         _, image, parents = args
@@ -66,6 +68,7 @@ def get_unconfounded_datasets(dataset: tf.data.Dataset, parent_dims: Dict[str, i
         return img_encode_fn(image), parents
 
     dataset = dataset.map(lambda d: {key: encode(*value) for key, value in d.items()})
+    dataset = dataset.batch(batch_size, drop_remainder=True)
     dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
     dataset = tfds.as_numpy(dataset)
     return dataset, marginals
